e997f2fd128b363a757d7625c32aff2a85978526
Marc Fernandez <marc.fernandezg@gmail.com>
2016-04-13 23:10:48 -0700

    fixup Feed

--- lib/criticism/feed.rb
+++ lib/criticism/feed.rb
@@ -1,5 +1,20 @@
 module Criticism
   class Feed
 
+    attr_reader :app_id, :country, :language
+
+    def initialize(app_id: , country: 'us', language: 'en')
+      @app_id = app_id
+      @country = country
+      @language = language
+    end
+
+  private
+
+    def url
+      "https://itunes.apple.com/#{country}/rss/customerreviews/id=#{app_id}/"\
+      "sortBy=mostRecent/xml?l=#{language}"
+    end
+
   end
 end
---
+++ spec/criticism/feed_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+
+describe Criticism::Feed do
+
+  describe '#new' do
+    let(:app_id) { 1234567 }
+    let(:country) { 'es' }
+    let(:language) { 'it' }
+
+    context 'arguments' do
+      context 'all arguments present' do
+        it 'sets app_id attribute' do
+          expect(
+            Criticism::Feed.new(
+              app_id:   app_id,
+              country:  country,
+              language: language
+            ).app_id
+          ).to eq(
+            app_id
+          )
+        end
+
+        it 'sets country attribute' do
+          expect(
+            Criticism::Feed.new(
+              app_id:   app_id,
+              country:  country,
+              language: language
+            ).country
+          ).to eq(
+            country
+          )
+        end
+
+        it 'sets language attribute' do
+          expect(
+            Criticism::Feed.new(
+              app_id:   app_id,
+              country:  country,
+              language: language
+            ).language
+          ).to eq(
+            language
+          )
+        end
+      end
+
+      context 'language argument missing' do
+        it 'sets default language attribute' do
+          expect(
+            Criticism::Feed.new(app_id: app_id, country: country).language
+          ).to eq(
+            'en'
+          )
+        end
+      end
+
+      context 'country argument missing' do
+        it 'sets default language attribute' do
+          expect(
+            Criticism::Feed.new(app_id: app_id, language: language).country
+          ).to eq(
+            'us'
+          )
+        end
+      end
+    end
+  end
+end
