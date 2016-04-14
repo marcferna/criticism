c6a41a82341ecdca32b09b5052f953ae2f8e0b27
Marc Fernandez <marc.fernandezg@gmail.com>
2016-04-13 22:28:24 -0700

    First commit

---
+++ lib/criticism.rb
@@ -0,0 +1,5 @@
+require "criticism/version"
+
+module Criticism
+  # Your code goes here...
+end
---
+++ spec/criticism_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Criticism do
+  it 'has a version number' do
+    expect(Criticism::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
